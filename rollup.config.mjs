13b45f9999ce2329d296bab700d689cf9a0e88a5
Leon Sorokin <leeoniya@gmail.com>
2025-08-12 14:47:59 -0500

    something works

---
+++ rollup.config.mjs
@@ -0,0 +1,29 @@
+import terser from '@rollup/plugin-terser';
+
+const terserOpts = {
+	compress: {
+		inline: 0,
+		passes: 2,
+		keep_fargs: false,
+		pure_getters: true,
+		unsafe: true,
+		unsafe_comps: true,
+		unsafe_math: true,
+		unsafe_undefined: true,
+	},
+};
+
+export default [
+  {
+    input: "./src/uMarks.mjs",
+    output: {
+      name: 'uMarks',
+      file: "./dist/uMarks.iife.min.js",
+      format: "iife",
+      strict: true,
+    },
+    plugins: [
+      terser(terserOpts),
+    ],
+  },
+];
---
+++ src/uMarks.mjs
@@ -0,0 +1,187 @@
+let { abs, sin, cos, pow, sqrt, acos, PI } = Math;
+
+let rad = deg => (deg * PI) / 180;
+let deg = rad => (rad * 180) / PI;
+let safeAcos = x => x < -1 ? PI : x > 1 ? 0 : acos(x);
+let sqrtAbs = x => sqrt(abs(x));
+
+let vectorAngle = ([ux, uy], [vx, vy]) => {
+  let sign = ux * vy - uy * vx < 0 ? -1 : 1;
+  let ua = sqrtAbs(ux * ux + uy * uy);
+  let va = sqrtAbs(vx * vx + vy * vy);
+  let dot = ux * vx + uy * vy;
+
+  return sign * safeAcos(dot / (ua * va));
+}
+
+// https://www.w3.org/TR/SVG/implnote.html#ArcConversionCenterToEndpoint
+// impl adapted and tweaked from
+// https://github.com/zjffun/svg-arc-center-endpoint
+// https://observablehq.com/@awhitty/svg-2-elliptical-arc-to-canvas-path2d
+function endpointToCenter(x1, y1, rx, ry, rot, fa, fs, x2, y2) {
+  let phi = rad(rot);
+
+  let sinPhi = sin(phi);
+  let cosPhi = cos(phi);
+
+  // Step 1: simplify through translation/rotation
+  let x1_ = (cosPhi * (x1 - x2)) / 2 + (sinPhi * (y1 - y2)) / 2;
+  let y1_ = (-sinPhi * (x1 - x2)) / 2 + (cosPhi * (y1 - y2)) / 2;
+  let x1_p2 = pow(x1_, 2);
+  let y1_p2 = pow(y1_, 2);
+
+  // B.2.5. Correction of out-of-range radii
+  {
+    // Step 1: Ensure radii are non-zero
+    // if (rx === 0 || ry === 0) {
+    //     return { cx: (x1 + x2) / 2, cy: (y1 + y2) / 2, theta: 0, dTheta: 0 };
+    // }
+    // Step 2: Ensure radii are positive
+    rx = abs(rx);
+    ry = abs(ry);
+    // Step 3: Ensure radii are large enough
+    let L = x1_p2 / pow(rx, 2) + y1_p2 / pow(ry, 2);
+
+    if (L > 1) {
+      rx = sqrtAbs(L) * rx;
+      ry = sqrtAbs(L) * ry;
+    }
+  }
+
+  // Step 2 + 3: compute center
+  let rxp2 = pow(rx, 2);
+  let ryp2 = pow(ry, 2);
+  let sign = fa === fs ? -1 : 1;
+
+  /**
+   * `pow(sqrt(L) * rx, 2)` will less than `L * pow(rx, 2)` when we run B.2.5. Correction of out-of-range radii
+   * so below value will be negative, we need use abs to fix it
+   */
+  let M = sqrtAbs((rxp2 * ryp2 - rxp2 * y1_p2 - ryp2 * x1_p2) / (rxp2 * y1_p2 + ryp2 * x1_p2)) * sign;
+  let cx_ = (M * (rx * y1_)) / ry;
+  let cy_ = (M * (-ry * x1_)) / rx;
+  let cx = cosPhi * cx_ - sinPhi * cy_ + (x1 + x2) / 2;
+  let cy = sinPhi * cx_ + cosPhi * cy_ + (y1 + y2) / 2;
+
+  // Step 4: compute θ and dθ
+  let theta = deg(vectorAngle([1, 0], [(x1_ - cx_) / rx, (y1_ - cy_) / ry]));
+  let dTheta = deg(vectorAngle([(x1_ - cx_) / rx, (y1_ - cy_) / ry], [(-x1_ - cx_) / rx, (-y1_ - cy_) / ry])) % 360;
+
+  if (fs === 0 && dTheta > 0)
+    dTheta -= 360;
+
+  if (fs === 1 && dTheta < 0)
+    dTheta += 360;
+
+  return [
+    cx,
+    cy,
+    rx,
+    ry,
+    phi,
+    rad(theta),
+    rad(theta + dTheta),
+    dTheta < 0
+  ];
+}
+
+let reflect = (cx, cy, pcx, pcy) => [pcx == null ? cx : cx * 2 - pcx, pcy == null ? cy : cy * 2 - pcy];
+let chunk = (arr, size) =>
+  arr.reduce((acc, _, i) => {
+    i % size == 0 && acc.push(arr.slice(i, i + size));
+    return acc;
+  }, []);
+
+// TODO:
+
+// relative commands (test mixed)
+// Path2D or ctx2d (w/z sub-paths)
+// render to svg to getBBox
+// offset for centering on data point
+// pass in x coord, y coord
+// rotation? pass in along with coords + scale
+// use isPointInPath hit testing, with on-demand Path2D on hover
+// optimize circles, rects, triangle up/down, diamond, maybe plus, cross
+// optimize poly with segment cache scaled up? point-in-polygon-hao
+
+export function createPathDrawer(pathStr) {
+  // bbox for svg is calculated with getBBox
+  // center shift is established via scale * -bbox.width/2, scale * -bbox.height/2
+  // then unscaled x and y data coord shifts are also applied to all coords
+
+  let cmds = pathStr.match(/[A-Z][^A-Z]*/g).flatMap(cmd => {
+    let c = cmd[0];
+
+    let numArgs = (
+      c == 'V' || c == 'H'             ? 1 :
+      c == 'M' || c == 'L' || c == 'T' ? 2 :
+      c == 'Q' || c == 'S'             ? 4 :
+      c == 'C'                         ? 6 :
+      c == 'A'                         ? 7 :
+      0
+    );
+
+    let args = cmd.slice(1).trim().split(/[ ,]/).filter(p => p !== '').map(Number);
+
+    return args.length > 0 ? chunk(args, numArgs).map((a, i) => [(c == 'M' && i > 0 ? 'L' : c), ...a]) : [[c]];
+  });
+
+  let x = 0;
+  let y = 0;
+
+  // prev control point
+  // C, S
+  let _cx = null;
+  let _cy = null;
+  // Q, T
+  let _qx = null;
+  let _qy = null;
+
+  // s (scale)
+  let funcBody = 's ??= 1;';
+
+  for (let i = 0; i < cmds.length; i++) {
+    let [type, ...p] = cmds[i];
+
+    funcBody += '\n';
+
+    switch (type) {
+      case 'T':
+        [_qx, _qy] = reflect(x, y, _qx, _qy);
+        funcBody += `ctx.quadraticCurveTo(s * ${_qx}, s * ${_qy}, s * ${x = p[0]}, s * ${y = p[1]});`;
+        _cx = _cy = null;
+        break;
+      case 'S':
+        [_cx, _cy] = reflect(x, y, _cx, _cy);
+        funcBody += `ctx.bezierCurveTo(s * ${_cx}, s * ${_cy}, s * ${_cx = p[0]}, s * ${_cy = p[1]}, s * ${x = p[2]}, s * ${y = p[3]});`
+        _qx = _qy = null;
+        break;
+      case 'A':
+        let p2 = endpointToCenter(x, y, ...p);
+        x = p[5];
+        y = p[6];
+        funcBody += `ctx.ellipse(s * ${p2[0]}, s * ${p2[1]}, s * ${p2[2]}, s * ${p2[3]}, ${p2[4]}, ${p2[5]}, ${p2[6]}, ${p2[7]});`;
+        break;
+      case 'Z':
+        funcBody += `ctx.closePath();`;
+        // funcBody += `ctx.beginPath();`;
+        break;
+      default:
+        funcBody += (
+          type == 'M' ? `ctx.moveTo(s * ${x = p[0]}, s * ${y = p[1]});` :
+          type == 'L' ? `ctx.lineTo(s * ${x = p[0]}, s * ${y = p[1]});` :
+          type == 'V' ? `ctx.lineTo(s * ${x}, s * ${y = p[0]});` :
+          type == 'H' ? `ctx.lineTo(s * ${x = p[0]}, s * ${y});` :
+          type == 'C' ? `ctx.bezierCurveTo(s * ${p[0]}, s * ${p[1]}, s * ${_cx = p[2]}, s * ${_cy = p[3]}, s * ${x = p[4]}, s * ${y = p[5]});` :
+          type == 'Q' ? `ctx.quadraticCurveTo(s * ${_qx = p[0]}, s * ${_qy = p[1]}, s * ${x = p[2]}, s * ${y = p[3]});` :
+          ''
+        );
+    }
+
+    if (type != 'C' && type != 'Q' && type != 'S' && type != 'T')
+      _cx = _cy = _qx = _qy = null;
+  }
+
+  // TODO: introduce shift dx, dy to draw at dynamic coords
+  return new Function('ctx', 's', 'x', 'y', funcBody);
+}
